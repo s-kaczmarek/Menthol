79fa4a4d2af6f6b82edf433b99f58ce3d8564be3
Sławomir Kaczmarek <skaczmarek88@gmail.com>
2017-12-05 23:37:51 +0100

    loging function added

--- functions.sh
+++ functions.sh
@@ -1,17 +1,57 @@
 #!/bin/bash
 
+# GLOBAL VARIABLES
+
+TIME=`date '+%d/%m/%Y %H:%M:%S'`
+MESSAGE=""  # override this variable each time you want to add log entry  
+
 # FUNCTIONS
+
+### REMEMBER TO DELETE ALL #TEST
 
 # Recommended packages installation
 
-enable_recommended_packages_installation {
+first_run_log() {
 
-sudo mv -v /etc/apt/apt.conf.d/00recommends /etc/apt/apt.conf.d/00recommends.disabled
-# This operation will enable recommended packages installation. You can easyly revert this opperation by changing file name to previous one.
+touch ~/.menthol/log/menthol.log
+#time=`date '+%d/%m/%Y %H:%M:%S'`
+echo "$TIME - FIRST RUN" >> ~/.menthol/log/menthol.log
 
 }
 
-disable_recommended_packages_installation {
+log_entry() {
+
+echo "$TIME - $MESSAGE" >> ~/.menthol/log/menthol.log
+
+}
+
+enable_recommended_packages_installation() {
+
+echo -e "\e[0;36m1. Enable installation of Recommended packages.\e[0m"
+echo -e ""
+echo -e "\e[0;36mRecommended packages are additional packages that are sometimes very important for newly installed programs to work. Linux Mint by default does not install additional packages as Ubuntu does. In some some situations it may endup with not working program that you already installed\e[0m" #TODO work on this description
+echo -e ""
+echo -e "\e[0;36mWould you like to procede? [y/n]\e[0m"
+
+while read y_n; do
+    case "$y_n" in
+     [Yy]* ) #TEST sudo mv -v /etc/apt/apt.conf.d/00recommends /etc/apt/apt.conf.d/00recommends.disabled
+             # This operation will enable recommended packages installation. You can easyly revert this opperation by changing file name to previous one.
+             MESSAGE="Recommended packages installation enabled!"
+             echo -e "\e[0;92m$MESSAGE\e[0m"
+             log_entry
+             break ;;                                                                              
+     [Nn]* ) MESSAGE="Recommended packages installation skipped"
+             echo -e "\e[0;92m$MESSAGE\e[0m"
+             log_entry
+             break ;;  
+          *) echo -e "\e[0;31m Wrong input! Type y or n \e[0m" ;        
+    esac
+done
+
+}
+
+disable_recommended_packages_installation() {
 
 sudo mv -v /etc/apt/apt.conf.d/00recommends.disabled /etc/apt/apt.conf.d/00recommends
 # This operation will disable recommended packages installation. Use it when you decide to keep default linux Mint settings.
@@ -21,23 +61,28 @@ echo -e "\e[0;32mrecommended packages installation enabled \e[0m"
 
 # Update packages list & packages
 
-update_packages_list {
+update_packages() {
+
+echo -e "\e[0;36m2. Updating installed packages.\e[0m"
+echo -e ""
+echo -e "\e[0;36mNow we will simply update all installed programs.\e[0m"
+read -n1 -r -p "Press any key to continue..." key
 
 sudo apt update
-echo -e "\e[0;32mlist of available packages updated \e[0m"
-
-}
-
-update_packages {
-
+MESSAGE="List of available packages updated!"
+echo -e "\e[0;92m$MESSAGE\e[0m"
+log_entry
 sudo apt upgrade
 sudo apt dist-upgrade
+MESSAGE="Packages updated!"
+echo -e "\e[0;92m$MESSAGE\e[0m"
+log_entry
 
 }
 
 # Update manager settings
 
-update_manager_settings {
+update_manager_settings() {
 
 echo -e "\e[0;32mNow the update manager will pop up, please change update policy to: -Let me review sensitive updates- and change mirrors for fastest for you. When you finnish, please close update manager and continue \e[0m"
 gksudo mintupdate
@@ -47,7 +92,7 @@ read -n1 -r -p "Press any key to continue..." key
 
 # Drivers
 
-driver_manager_settings {
+driver_manager_settings() {
 
 echo -e "\e[0;32m now the driver manager will pop up, please review if you have any drivers available, enable them if you want, enable microcode (this is optional, recommended only when you have noticed some issues with CPU). If you are being offered several versions of Nvidia drivers, start with number one, and if you notice some issues later on, change to number 2, 3 etc. When finnished, please close update manager and continue \e[0m" 
 driver-manager
--- menthol.sh
+++ menthol.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+source ./functions.sh
+
 clear
 
-#LOGO
+#TODO - LOGO
 
 #check if this is first run
 if [ ! -e ~/.menthol/menthol.log ]; then
@@ -14,21 +16,45 @@ if [ ! -e ~/.menthol/menthol.log ]; then
     echo -e "\e[0;36msetup automounting of additional drives and restore settings for thunderbird.\e[0m"
     echo -e "\e[0;36mAll of the steps performed with this program are easy to revert or just skip.\e[0m"
     echo -e "\e[0;36mIf you are not sure that you like some of them, simply run this program once again\e[0m"
-    echo -e "\e[0;36mand it will show you menu where you can decide which one of them you want to revert to defaults.\e[0m"
+    echo -e "\e[0;36mand it will show you menu where you can decide which one of them you want to revert\e[0m"
+    echo -e "\e[0;36mto defaults.\e[0m"
     echo -e ""
-    echo -e "\e[0;36mProgram requires internet connection, please make sure, that computer is connected to the internet and press any key to continue.\e[0m"
+    echo -e "\e[0;36mProgram requires internet connection, please make sure, that computer is connected to\e[0m"
+    echo -e "\e[0;36mthe internet and press any key to continue.\e[0m"
     echo -e ""
     read -n1 -r -p "Press any key to continue..." key
     
     #check internet connection
     if ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
-        echo "\e[0;36mComputer is connected to the internet!\e[0m"
+        echo -e "\e[0;92mComputer is connected to the internet! We are ready to procede.\e[0m"
+        # I BASIC SYTEM SETTINGS
+        echo ""
+        echo -e "\e[0;36mI. BASIC SYSTEM SETTINGS\e[0m"
+        echo ""
+        echo -e "\e[0;36mIn this step we will tune system to perform better in case of installing new packages and we will prevent it from using your ssd drive to much, so it will live longer. Remember that all of the steps are easy to revert and you will be able to do so by running this scrip again. For now please follow instructions presented in this wizard.\e[0m"
+        echo ""
+        echo -e "\e[0;36mIn this part we will make steps presented in the list below:\e[0m"
+        # I BASIC SYSTEM SETTINGS - LIST
+        echo ""
+        echo -e "\e[0;36m    1. Enabling of the recommended packages installation\e[0m"
+        echo -e "\e[0;36m    2. Updating installed packages\e[0m"
+        echo -e "\e[0;36m    3. Update manager settings\e[0m"
+        echo -e "\e[0;36m    4. Driver manager settings\e[0m"
+        echo -e ""
+        read -n1 -r -p "Press any key to continue..." key
+        
+        first_run_log
+        
+        # Recomended packages installation
+        enable_recommended_packages_installation
+        update_packages
+        
     else
-        echo "\e[0;31mWe have problem with internet connection. Please reconnect and run script again.\e[0m"
+        echo -e "\e[0;31;7mWe have problem with internet connection. Please reconnect and run script again.\e[0m"
     fi
     
     
     
 else
-    echo "menthol.log does exist"
+    echo "menthol.log does exist" #second run v
 fi
