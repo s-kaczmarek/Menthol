03f81a0152bb1539a579c59631afc655b61d9488
Sławomir Kaczmarek <skaczmarek88@gmail.com>
2017-12-29 21:01:37 +0100

    installation file update

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+# Chcek if .menthol already exists
+# If no than:
+
+install_all() {
+    mv -v ../Menthol ~/.menthol
+    cd ~/.menthol
+    # Check if file exists in location and ask for overwrite v
+    cp ~/.menthol/resources/desktop/Menthol.desktop ~/.local/share/applications
+}
 mkdir ~/.menthol
+# if yeas than:
+# ask what to do
